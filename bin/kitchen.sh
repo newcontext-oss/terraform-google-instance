d60f33d97b3d13b092a944a58a2004aacb5a4cd3
nictrix <nickwillever@gmail.com>
2018-05-24 21:11:16 -0700

    add cache

--- bin/kitchen.sh
+++ bin/kitchen.sh
@@ -4,20 +4,20 @@
 tar -zxf ci.tar.gz
 
 # Add binaries to bin directory
-mkdir -p bin;
-export PATH=$PATH:bin:google-cloud-sdk/bin
+mkdir -p vendor/bin
+export PATH=$PATH:vendor/bin:vendor/google-cloud-sdk/bin
 
 # Install Terraform
 wget -q https://releases.hashicorp.com/terraform/0.11.7/terraform_0.11.7_linux_amd64.zip
 unzip terraform_0.11.7_linux_amd64.zip
 rm terraform_0.11.7_linux_amd64.zip
-mv terraform bin/
+mv terraform vendor/bin/
 
 # Install gcloud command line client
 wget -q https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-202.0.0-linux-x86_64.tar.gz
-tar -zxf google-cloud-sdk-*-linux-x86_64.tar.gz
+tar -zxf google-cloud-sdk-*-linux-x86_64.tar.gz -C vendor
 rm google-cloud-sdk-*-linux-x86_64.tar.gz
-./google-cloud-sdk/install.sh -q
+./vendor/google-cloud-sdk/install.sh -q
 
 # Authenticate using the credentials.json
 gcloud auth activate-service-account --key-file credentials.json
--- bin/terraform_checks.sh
+++ bin/terraform_checks.sh
@@ -1,19 +1,13 @@
 #!/usr/bin/env bash
 
-# Decompress sensitive files
-tar -zxf ci.tar.gz
-
 # Add binaries to bin directory
-mkdir -p bin;
-export PATH=$PATH:bin
+mkdir -p vendor/bin
+export PATH=$PATH:vendor/bin
 
 # Install Terraform
 wget -q https://releases.hashicorp.com/terraform/0.11.7/terraform_0.11.7_linux_amd64.zip
 unzip terraform_0.11.7_linux_amd64.zip
 rm terraform_0.11.7_linux_amd64.zip
-mv terraform bin/
-
-yes | ssh-keygen -f ubuntu -N '' >/dev/null
+mv terraform vendor/bin/
 
 terraform fmt
-terraform validate
